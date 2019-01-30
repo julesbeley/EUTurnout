d53fab78cb982a0f06934a43180ea2b6d8830af8
Jules Beley <jules.beley@gmail.com>
2019-01-30 14:37:53 +0100

    Converted columns and removed commas

--- IDEATurnout.R
+++ IDEATurnout.R
@@ -1,21 +1,18 @@
 rm(list = ls())
 library(tidyverse)
-turnout <- read.csv("./Turnout.csv", na.strings = "")
+turnout <- read.csv("./Turnout.csv", na.strings = "", stringsAsFactors = FALSE)
 turnout %>% 
-  filter(`Election.type` %in% "EU Parliament") %>% 
+  mutate(`Year` = as.numeric(`Year`)) %>% 
+  mutate(`Voter.Turnout` = parse_number(`Voter.Turnout`)) %>% 
+  mutate(`Registration` = as.numeric(gsub(",", "", `Registration`))) %>% 
+  mutate(`Voting.age.population` = as.numeric(gsub(",", "", `Voting.age.population`))) %>% 
+  mutate(`Population` = as.numeric(gsub(",", "", `Population`))) %>% 
   rename(country = ï..Country) %>% 
   rename(type = Election.type) %>% 
   rename(year = Year) %>% 
   rename(turnout = Voter.Turnout) %>% 
   rename(registered = Registration) %>% 
   rename(votingagepop = Voting.age.population) %>% 
-  rename(population = Population) -> eu
-turnout %>% 
-  filter(`Election.type` %in% "Parliamentary") %>% 
-  rename(country = ï..Country) %>% 
-  rename(type = Election.type) %>% 
-  rename(year = Year) %>% 
-  rename(turnout = Voter.Turnout) %>% 
-  rename(registered = Registration) %>% 
-  rename(votingagepop = Voting.age.population) %>% 
-  rename(population = Population)-> national
+  rename(population = Population) %>% 
+  arrange(`year`) -> turnout
+
