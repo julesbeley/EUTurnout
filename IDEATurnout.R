7fd49f91a9a2232d037fea8823d3012b104e2168
Jules Beley <jules.beley@gmail.com>
2019-01-30 13:44:50 +0100

    Cleaning up the data

--- IDEATurnout.R
+++ IDEATurnout.R
@@ -1,2 +1,21 @@
+rm(list = ls())
 library(tidyverse)
-turnout <- read.csv("./Turnout.csv")
+turnout <- read.csv("./Turnout.csv", na.strings = "")
+turnout %>% 
+  filter(`Election.type` %in% "EU Parliament") %>% 
+  rename(country = ï..Country) %>% 
+  rename(type = Election.type) %>% 
+  rename(year = Year) %>% 
+  rename(turnout = Voter.Turnout) %>% 
+  rename(registered = Registration) %>% 
+  rename(votingagepop = Voting.age.population) %>% 
+  rename(population = Population) -> eu
+turnout %>% 
+  filter(`Election.type` %in% "Parliamentary") %>% 
+  rename(country = ï..Country) %>% 
+  rename(type = Election.type) %>% 
+  rename(year = Year) %>% 
+  rename(turnout = Voter.Turnout) %>% 
+  rename(registered = Registration) %>% 
+  rename(votingagepop = Voting.age.population) %>% 
+  rename(population = Population)-> national
