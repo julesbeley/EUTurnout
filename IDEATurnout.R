21bc39005ad7a2048846a4a18e126b600c745f6d
Jules Beley <jules.beley@gmail.com>
2019-02-06 00:18:06 +0100

    Layout

--- IDEATurnout.R
+++ IDEATurnout.R
@@ -1,19 +1,22 @@
 rm(list = ls())
+
 library(tidyverse)
 library(ggpubr)
 library(grid)
 library(gridExtra)
 library(mapproj)
+
 turnout <- read.csv("./Turnout.csv",
                     na.strings = "",
                     stringsAsFactors = FALSE)
+
 turnout %>%
   mutate(`Year` = as.numeric(`Year`)) %>%
   mutate(`Voter.Turnout` = parse_number(`Voter.Turnout`)) %>%
   mutate(`Registration` = as.numeric(gsub(",", "", `Registration`))) %>%
   mutate(`Voting.age.population` = as.numeric(gsub(",", "", `Voting.age.population`))) %>%
   mutate(`Population` = as.numeric(gsub(",", "", `Population`))) %>%
-  rename(country =    ï..Country) %>%
+  rename(country =     ï..Country) %>%
   rename(type = Election.type) %>%
   rename(year = Year) %>%
   rename(turnout = Voter.Turnout) %>%
@@ -23,24 +26,27 @@ turnout %>%
   arrange(`year`) -> turnout
 
 eumap = function(date) {
-  par(mfrow = c(3,3))
   library(tidyverse)
   library(maps)
   library(ggplot2)
   library(viridis)
+  
   turnout %>%
     filter(`year` %in% date) %>%
     filter(type %in% "EU Parliament") %>%
     select(`country`, `turnout`) %>%
     rename(region = country) -> turnout
+  
   world <- map_data("world")
   world %>%
     mutate(`region` = recode(`region`, "UK" = "United Kingdom")) -> world
+  
   tojoin <- data.frame(region = names(table(world$region)))
   all <- full_join(turnout, tojoin, by = "region")
   all %>%
     arrange(`region`) -> all
   mapbig <- inner_join(world, all, by = "region")
+  
   worldmap <- ggplot() + theme(
     panel.background = element_rect(fill = "lightcyan1",
                                     color = NA),
@@ -51,24 +57,31 @@ eumap = function(date) {
     axis.title.x = element_blank(),
     axis.title.y = element_blank()
   )
-  europe <- worldmap + coord_map("lambert", 
-                                 parameters = c(30, 43), 
-                                 xlim = c(-10, 35),
-                                 ylim = c(35, 70.1))
-  europe2 <- europe + geom_polygon(data = mapbig,
-                                   aes(fill = turnout,
-                                       x = long,
-                                       y = lat,
-                                       group = group),
-                                   color = "grey30") +
-    scale_fill_viridis_c(limits = c(13,93),
-                         option = "plasma",
-                         direction = -1,
-                        guide = "none",
-                        na.value = "grey70") +
-    annotate("text", -10, 68, label = as.character(date), size = 35)
-  europe2
+  europe <- worldmap + coord_map(
+    "lambert",
+    parameters = c(30, 43),
+    xlim = c(-10, 35),
+    ylim = c(35, 70.1)
+  )
+  europe <- europe + geom_polygon(data = mapbig,
+                                  aes(
+                                    fill = turnout,
+                                    x = long,
+                                    y = lat,
+                                    group = group
+                                  ),
+                                  color = "grey30")
+  europe <- europe + scale_fill_viridis_c(
+    limits = c(13, 93),
+    option = "plasma",
+    direction = -1,
+    guide = "none",
+    na.value = "grey70"
+  )
+  europe <-
+    europe + annotate("text", -10, 68, label = as.character(date), size = 35)
 }
+
 "one" <- eumap(1979)
 "two" <- eumap(1984)
 "three" <- eumap(1989)
@@ -83,14 +96,17 @@ turnout %>%
   filter(type %in% "EU Parliament") %>%
   select(`country`, `turnout`) %>%
   rename(region = country) -> turnout
+
 world <- map_data("world")
 world %>%
   mutate(`region` = recode(`region`, "UK" = "United Kingdom")) -> world
+
 tojoin <- data.frame(region = names(table(world$region)))
 all <- full_join(turnout, tojoin, by = "region")
 all %>%
   arrange(`region`) -> all
 mapbig <- inner_join(world, all, by = "region")
+
 worldmap <- ggplot() + theme(
   panel.background = element_rect(fill = "lightcyan1",
                                   color = NA),
@@ -103,37 +119,65 @@ worldmap <- ggplot() + theme(
   legend.text = element_text(size = 100),
   legend.spacing.x = unit(10, "mm"),
   legend.title = element_text(size = 155)
-  )
+)
+
 europe <- worldmap + coord_fixed(xlim = c(-9, 35),
                                  ylim = c(36, 70.1),
                                  ratio = 1.5)
-europe2 <- europe + geom_polygon(data = mapbig,
-                                 aes(fill = turnout,
-                                     x = long,
-                                     y = lat,
-                                     group = group),
-                                 color = "grey50") +
-  scale_fill_viridis_c(limits = c(13,93),
-                       option = "plasma",
-                       direction = -1,
-                       na.value = "grey70",
-                       breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100),
-                       guide = guide_colorbar(barheight = unit(600, units = "mm"),
-                                              barwidth = unit(45, units = "mm"),
-                                              title = "%"))
-legend <- get_legend(europe2)
+
+europe <- europe + geom_polygon(data = mapbig,
+                                aes(
+                                  fill = turnout,
+                                  x = long,
+                                  y = lat,
+                                  group = group
+                                ),
+                                color = "grey50")
+
+europe <- europe + scale_fill_viridis_c(
+  limits = c(13, 93),
+  option = "plasma",
+  direction = -1,
+  na.value = "grey70",
+  breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100),
+  guide = guide_colorbar(
+    barheight = unit(600, units = "mm"),
+    barwidth = unit(45, units = "mm"),
+    title = "%"
+  )
+)
+
+legend <- get_legend(europe)
 legend <- as_ggplot(legend)
+
 lay <- rbind(c(NA, 1, 2, 3, 4, 9), c(NA, 5, 6, 7, 8, 9))
-png("./participation.png", height = 4300, width = 10000)
-grid.arrange(one, two, three, four, five, six, seven, eight, legend,
-             layout_matrix = lay,
-             top = textGrob("Voter turnout in EU elections, 1979-2014",
-                            gp = gpar(fontsize = 200,
-                                      fontface = "bold")),
-             bottom = textGrob("Source: International IDEA. Computed by J. Beley (2018).",
-                               gp = gpar(fontsize = 100,
-                                         fontface = 3),
-                               hjust = 0,
-                               vjust = 0,
-                               x = 0.575))
+
+png("./participation.png", height = 4200, width = 10000)
+
+grid.arrange(
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  legend,
+  layout_matrix = lay,
+  top = textGrob(
+    "Voter turnout in EU elections, 1979-2014",
+    gp = gpar(fontsize = 200,
+              fontface = "bold"),
+    y = 0.5
+  ),
+  bottom = textGrob(
+    "Source: International IDEA. Computed by J. Beley (2018).",
+    gp = gpar(fontsize = 100,
+              fontface = 3),
+    hjust = 0,
+    vjust = 0,
+    x = 0.575
+  )
+)
 dev.off()
