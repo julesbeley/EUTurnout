e433a47fcd9408463c9df67d0032d15f4560d1f7
Jules Beley <jules.beley@gmail.com>
2019-02-10 19:35:33 +0100

    New lines script

---
+++ Lines.R
@@ -0,0 +1,84 @@
+rm(list = ls())
+library(tidyverse)
+install.packages("RColorBrewer")
+library(RColorBrewer)
+
+turnout <- read.csv("./Turnout.csv",
+                    na.strings = "",
+                    stringsAsFactors = FALSE)
+
+turnout %>%
+     mutate(`Year` = as.numeric(`Year`)) %>%
+     mutate(`Voter.Turnout` = parse_number(`Voter.Turnout`)) %>%
+     mutate(`Registration` = as.numeric(gsub(",", "", `Registration`))) %>%
+     mutate(`Voting.age.population` = as.numeric(gsub(",", "", `Voting.age.population`))) %>%
+     mutate(`Population` = as.numeric(gsub(",", "", `Population`))) %>%
+     rename(country =       ï..Country) %>%
+     rename(type = Election.type) %>%
+     rename(year = Year) %>%
+     rename(turnout = Voter.Turnout) %>%
+     rename(registered = Registration) %>%
+     rename(votingagepop = Voting.age.population) %>%
+     rename(population = Population) %>%
+     filter(`type` %in% "EU Parliament") %>%
+     filter(`country` %in% c(
+          "Belgium",
+          "Luxembourg",
+          "Italy",
+          "Germany",
+          "Ireland",
+          "France",
+          "Netherlands",
+          "Denmark",
+          "United Kingdom"
+     )) %>% 
+     arrange(desc(`turnout`)) -> turnout
+
+subset(turnout, `year` == 1979)$country
+
+
+graph <- ggplot(turnout) + 
+     geom_line(aes(x = `year`, y = `turnout`, color = `country`), size = 2.2) +
+     geom_point(aes(x = `year`, y = `turnout`, color = `country`),
+                shape = 1,
+                size = 6) +
+     geom_text(
+          size = 8,
+          data = subset(turnout, `year` == "1979") %>% slice(c(1,3,5,8,9)),
+          aes(
+               label = `country`,
+               colour = `country`,
+               x = `year` - 5,
+               y = `turnout`
+          )) +
+     geom_text(
+          size = 8,
+          data = subset(turnout, `year` == "2014") %>% slice(c(2,6,7,8)),
+          aes(
+               label = `country`, 
+               colour = `country`, 
+               x = `year` + 5,
+               y = `turnout`
+          )) +
+     theme(
+          plot.title = element_text(size = 30),
+          plot.caption = element_text(size = 15),
+          axis.text.x = element_text(size = 19, colour = "black"),
+          axis.text.y = element_text(size = 19, colour = "black"),
+          axis.title.y = element_blank(),
+          axis.title.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_blank(),
+          panel.background = element_blank(),
+          panel.grid.major.x = element_line(color = "grey"),
+          panel.grid.major.y = element_line(color = "brown2", linetype = 2)) +
+     scale_color_brewer(guide = "none", palette = "Paired") +
+     scale_x_continuous(limits = c(1970, 2025), breaks = seq(1979, 2014, 5)) +
+     scale_y_continuous(limits = c(0,100), labels = c("0%", "25%", "50%", "75%", "100%")) +
+     labs(title = "Participation in EU elections since 1979 among 'the Nine'",
+          caption = "Source: International IDEA. Computed by J. Beley (2018)")
+graph
+
+png("./graph.png", width = 1200, height = 700)
+graph
+dev.off()
