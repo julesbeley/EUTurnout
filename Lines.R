a3d1469c90ecd2e603fc0fa083bbacb9b240a5b6
Jules Beley <jules.beley@gmail.com>
2019-02-20 22:07:24 +0100

    Done

--- Lines.R
+++ Lines.R
@@ -40,34 +40,40 @@ turnout %>%
 
 for (i in (seq(1979, 2014, 5))) {
         turnout <- rbind(turnout, 
-              data.frame(country = "EU9",
+              data.frame(country = "Average",
                          year = i,
-                         turnout = round(sum(turnout2$voters[turnout2$year == i]) / sum(turnout2$registered[turnout2$year == i]), 2),
-                         registered = sum(turnout2$registered[turnout2$year == i]),
-                         voters = sum(turnout2$voters[turnout2$year == i])))
+                         turnout = round(sum(turnout$voters[turnout$year == i]) / sum(turnout$registered[turnout$year == i]), 2),
+                         registered = sum(turnout$registered[turnout$year == i]),
+                         voters = sum(turnout$voters[turnout$year == i])))
 }
 
 turnout %>% 
      arrange(`year`) -> turnout
 
-subset(turnout, `year` != 1979)
+pal <- brewer.pal(10, "Paired")
+pal[1] <- "#000000"
 
 graph <- ggplot(turnout) + 
-     geom_line(data = subset(turnout, `country` != "EU9"), 
-               aes(x = `year`, y = `turnout`, color = `country`), size = 1.6) +
-     geom_point(data = subset(turnout, `country` != "EU9"),
+     geom_line(data = subset(turnout, `country` != "Average"), 
+               aes(x = `year`, y = `turnout`, color = `country`), 
+               size = 1.6,
+               alpha = 0.7) +
+     geom_point(data = subset(turnout, `country` != "Average"),
                 aes(x = `year`, y = `turnout`, color = `country`),
                 shape = 19,
-                size = 6) +
-     geom_line(data = subset(turnout, `country` == "EU9"), 
-               aes(x = `year`, y = `turnout`, color = "black"), size = 1.6) +
-     geom_point(data = subset(turnout, `country` == "EU9"),
-                aes(x = `year`, y = `turnout`, color = "black"),
+                size = 5,
+                alpha = 0.7) +
+     geom_line(data = subset(turnout, `country` == "Average"), 
+               aes(x = `year`, y = `turnout`, color = `country`), 
+               size = 1.6,
+               linetype = "dotted") +
+     geom_point(data = subset(turnout, `country` == "Average"),
+                aes(x = `year`, y = `turnout`, color = `country`),
                 shape = 19,
-                size = 6) +
+                size = 5) +
      geom_text(
           size = 8,
-          data = subset(turnout, `year` == "1979", `country` != "EU9") %>% slice(c(1,3,5,8,9)),
+          data = subset(turnout, `year` == "1979") %>% slice(c(1,3,4,8,9)),
           aes(
                label = `country`,
                colour = `country`,
@@ -76,7 +82,16 @@ graph <- ggplot(turnout) +
           )) +
      geom_text(
           size = 8,
-          data = subset(turnout, `year` == "2014", `country` != "EU9") %>% slice(c(2,6,7,8)),
+          data = subset(turnout, `year` == "2014") %>% slice(c(2,5,7,8)),
+          aes(
+               label = `country`, 
+               colour = `country`, 
+               x = `year` + 5,
+               y = `turnout`
+          )) +
+     geom_text(
+          size = 8,
+          data = subset(turnout, `year` == "2014" & `country` == "Average"),
           aes(
                label = `country`, 
                colour = `country`, 
@@ -95,7 +110,7 @@ graph <- ggplot(turnout) +
           panel.background = element_blank(),
           panel.grid.major.x = element_line(color = "grey"),
           panel.grid.major.y = element_line(color = "brown2", linetype = 2)) +
-     scale_color_brewer(guide = "none", palette = "Paired") +
+     scale_color_manual(guide = "none", values = pal, aesthetics = "colour") +
      scale_x_continuous(limits = c(1970, 2025), breaks = seq(1979, 2014, 5)) +
      scale_y_continuous(limits = c(0,100), labels = c("0%", "25%", "50%", "75%", "100%")) +
      labs(title = "Participation in EU elections since 1979 among 'the Nine'",
