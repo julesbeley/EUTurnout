957e21a8d01db2f04a0996b44e4bab82a123b7b4
Jules Beley <jules.beley@gmail.com>
2019-02-23 19:22:03 +0100

    Comparing turnout in national and in EU parliamentary elections

--- compare.R
+++ compare.R
@@ -24,6 +24,51 @@ turnout %>%
 
 turnout
 
-ggplot(turnout) +
-     geom_point(data = turnout %>% filter(type == "Parliamentary"), aes(x = year, y = turnout)) 
+n <- ggplot(turnout) +
+     geom_point(data = turnout %>% filter(type == "Parliamentary"), 
+                aes(x = year, y = turnout),
+                size = 4,
+                alpha = 0.5) +
+     geom_point(data = turnout %>% filter(type == "EU Parliament"), 
+                aes(x = year, y = turnout),
+                color = "dodgerblue4",
+                size = 4,
+                alpha = 0.5) +
+     geom_smooth(data = turnout %>% filter(type == "Parliamentary"),
+                 aes(x = year, y = turnout),
+                 method = "lm", 
+                 colour = "black",
+                 alpha = 0.3) +
+     geom_smooth(data = turnout %>% filter(type == "EU Parliament"),
+                 aes(x = year, y = turnout),
+                 method = "lm", 
+                 colour = "dodgerblue4",
+                 alpha = 0.3) +
+     theme(
+          plot.title = element_text(size = 50, hjust = 0.5, vjust = 30),
+          plot.caption = element_text(size = 30, face = 3, vjust = -40),
+          axis.text.x = element_text(size = 30, colour = "black"),
+          axis.text.y = element_text(size = 30, colour = "black"),
+          axis.title.x = element_blank(),
+          axis.title.y = element_text(size = 38, colour = "black", vjust = 25),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_blank(),
+          panel.background = element_blank(),
+          panel.grid.major.x = element_line(color = "grey", size = 1.3),
+          panel.grid.major.y = element_line(color = "grey", size = 1.3),
+          plot.margin = unit(c(4.5, 7.5, 4.5, 7.5), "cm")) +
+     labs(title = "Turnout in EU and national parliamentary 
+elections with trend lines, 1979-2014",
+          caption = "Source: International IDEA. Computed by J. Beley (2019)") +
+     ylab("Turnout") +
+     scale_y_continuous(breaks = c(25, 50, 75, 100), 
+                        labels = c("25%", "50%", "75%", "100%"))
      
+png("./compare.png", width = 1600, height = 1000)
+n
+dev.off()
+
+lm(data = turnout %>% filter(type == "EU Parliament"), turnout ~ year) -> modelEU
+lm(data = turnout %>% filter(type == "Parliamentary"), turnout ~ year) -> modelNat
+summary(modelEU)
+summary(modelNat)
