90b7f4f172cf40b03a8bd91692bb3caaaf93298a
Jules Beley <jules.beley@gmail.com>
2019-02-26 20:36:05 +0100

    updated if clause in function

--- IDEATurnout.R
+++ IDEATurnout.R
@@ -102,6 +102,106 @@ eumap = function(date) {
           select(`country`, `turnout`) %>%
           rename(region = country) -> turnout
      
+     if (date < 1994) {
+          library(sf)
+          eastgermany1 <- read_sf("Berlin_AL4.geojson")
+          eastgermany2 <- read_sf("Berlin_AL6.geojson")
+          eastgermany <- rbind(eastgermany1, eastgermany2)
+          east <- list()
+          for (i in (1:dim(eastgermany)[1])) {
+               east[[i]] <- eastgermany[i,] 
+          }
+          EE1 <- as.data.frame(east[[1]]$geometry[[1]][[1]])
+          EE2 <- as.data.frame(east[[2]]$geometry[[1]][[1]][[1]])
+          EE3 <- as.data.frame(east[[3]]$geometry[[1]][[1]])
+          EE4 <- as.data.frame(east[[4]]$geometry[[1]][[1]][[1]])
+          EE5 <- as.data.frame(east[[5]]$geometry[[1]][[1]][[1]])
+          EE6 <- as.data.frame(east[[6]]$geometry[[1]][[1]][[1]])
+          EE7 <- as.data.frame(east[[7]]$geometry[[1]][[1]])
+          EE8 <- as.data.frame(east[[8]]$geometry[[1]][[1]])
+          EE9 <- as.data.frame(east[[9]]$geometry[[1]][[1]])
+          EE10 <- as.data.frame(east[[10]]$geometry[[1]][[1]])
+          EE11 <- as.data.frame(east[[11]]$geometry[[1]][[1]][[1]])
+          EE12 <- as.data.frame(east[[12]]$geometry[[1]][[1]])
+          EE13 <- as.data.frame(east[[13]]$geometry[[1]][[1]][[1]])
+          
+          colnames(EE1) = c("x", "y")
+          colnames(EE2) = c("x", "y")
+          colnames(EE3) = c("x", "y")
+          colnames(EE4) = c("x", "y")
+          colnames(EE5) = c("x", "y")
+          colnames(EE6) = c("x", "y")
+          colnames(EE7) = c("x", "y")
+          colnames(EE8) = c("x", "y")
+          colnames(EE9) = c("x", "y")
+          colnames(EE10) = c("x", "y")
+          colnames(EE11) = c("x", "y")
+          colnames(EE12) = c("x", "y")
+          colnames(EE13) = c("x", "y")
+          
+          rbind(EE1, EE2, EE3, EE4, EE5, EE6, EE7, EE8, EE9, EE10, EE11, EE12, EE13) -> EE
+          
+          dim(as.data.frame(concaveman::concaveman(as.matrix(EE))))[1]
+          
+          length = dim(as.data.frame(concaveman::concaveman(as.matrix(EE))))[1]
+          
+          matrix(ncol = 1, nrow = dim(as.data.frame(concaveman::concaveman(as.matrix(EE))))[1], 1628) -> u
+          
+          cbind(as.data.frame(concaveman::concaveman(as.matrix(EE))), 
+                as.vector(u), 
+                seq(from = 100965, to = 100965 + length - 1),
+                matrix(ncol = 1, nrow = length, "East Germany"),
+                matrix(ncol = 1, nrow = length, NA)) -> EG
+          
+          world <- map_data("world")
+          colnames(EG) <- colnames(world)
+          end <- rbind(world, EG)
+          world %>%
+               mutate(`region` = recode(`region`, "UK" = "United Kingdom")) -> world
+          
+          tojoin <- data.frame(region = names(table(world$region)))
+          all <- full_join(turnout, tojoin, by = "region")
+          all %>%
+               arrange(`region`) -> all
+          mapbig <- inner_join(world, all, by = "region")
+          
+          worldmap <- ggplot() + theme(
+               panel.background = element_rect(fill = "white",
+                                               color = NA),
+               panel.grid = element_line(colour = "royalblue"),
+               axis.text.x = element_blank(),
+               axis.text.y = element_blank(),
+               axis.ticks = element_blank(),
+               axis.title.x = element_blank(),
+               axis.title.y = element_blank()
+          )
+          europe <- worldmap + coord_map(
+               "lambert",
+               parameters = c(30, 43),
+               xlim = c(-10, 35),
+               ylim = c(35, 70.1)
+          )
+          europe <- europe + geom_polygon(
+               data = mapbig,
+               aes(
+                    fill = turnout,
+                    x = long,
+                    y = lat,
+                    group = group
+               ),
+               color = "grey30"
+          )
+          europe <- europe + scale_fill_viridis_c(
+               limits = c(13, 93),
+               option = "magma",
+               direction = -1,
+               guide = "none",
+               na.value = "grey70"
+          )
+          europe <- europe + annotate("text", -10, 68, label = as.character(date), size = 35)
+     }
+          
+     else {
      world <- map_data("world")
      world %>%
           mutate(`region` = recode(`region`, "UK" = "United Kingdom")) -> world
@@ -145,50 +245,8 @@ eumap = function(date) {
           guide = "none",
           na.value = "grey70"
      )
-     library(sf)
-     eastgermany1 <- read_sf("Berlin_AL4.geojson")
-     eastgermany2 <- read_sf("Berlin_AL6.geojson")
-     eastgermany <- rbind(eastgermany1, eastgermany2)
-     east <- list()
-     for (i in (1:dim(eastgermany)[1])) {
-          east[[i]] <- eastgermany[i,]
-     }
-     if (date < 1994) {
-          EE1 <- as.data.frame(east[[1]]$geometry[[1]][[1]])
-          EE2 <- as.data.frame(east[[2]]$geometry[[1]][[1]][[1]])
-          EE3 <- as.data.frame(east[[3]]$geometry[[1]][[1]])
-          EE4 <- as.data.frame(east[[4]]$geometry[[1]][[1]][[1]])
-          EE5 <- as.data.frame(east[[5]]$geometry[[1]][[1]][[1]])
-          EE6 <- as.data.frame(east[[6]]$geometry[[1]][[1]][[1]])
-          EE7 <- as.data.frame(east[[7]]$geometry[[1]][[1]])
-          EE8 <- as.data.frame(east[[8]]$geometry[[1]][[1]])
-          EE9 <- as.data.frame(east[[9]]$geometry[[1]][[1]])
-          EE10 <- as.data.frame(east[[10]]$geometry[[1]][[1]])
-          EE11 <- as.data.frame(east[[11]]$geometry[[1]][[1]][[1]])
-          EE12 <- as.data.frame(east[[12]]$geometry[[1]][[1]])
-          EE13 <- as.data.frame(east[[13]]$geometry[[1]][[1]][[1]])
-          
-          colnames(EE1) = c("x", "y")
-          colnames(EE2) = c("x", "y")
-          colnames(EE3) = c("x", "y")
-          colnames(EE4) = c("x", "y")
-          colnames(EE5) = c("x", "y")
-          colnames(EE6) = c("x", "y")
-          colnames(EE7) = c("x", "y")
-          colnames(EE8) = c("x", "y")
-          colnames(EE9) = c("x", "y")
-          colnames(EE10) = c("x", "y")
-          colnames(EE11) = c("x", "y")
-          colnames(EE12) = c("x", "y")
-          colnames(EE13) = c("x", "y")
-          
-          rbind(EE1, EE2, EE3, EE4, EE5, EE6, EE7, EE8, EE9, EE10, EE11, EE12, EE13) -> EE
-          
-          europe <- europe + 
-               geom_polygon(data = as.data.frame(concaveman::concaveman(as.matrix(EE))),
-                            aes(x = V1, y = V2))
-     }
      europe <- europe + annotate("text", -10, 68, label = as.character(date), size = 35)
+     }
 }
 
 "one" <- eumap(1979)
@@ -302,4 +360,4 @@ grid.arrange(
 dev.off()
 
 
-curl -f -o eastgermany.zip --url "https://wambachers-osm.website/boundaries/exportBoundaries?cliVersion=1.0&cliKey=6f4b0380-1ef1-4cdf-ae75-0ce88e32e15a&exportFormat=json&exportLayout=levels&exportAreas=water&union=false&selected=62422,62504,1739381,1739376,1739380,62405,1739377,62685,1431517,1739379,62607,62467,62366"
+#curl -f -o eastgermany.zip --url "https://wambachers-osm.website/boundaries/exportBoundaries?cliVersion=1.0&cliKey=6f4b0380-1ef1-4cdf-ae75-0ce88e32e15a&exportFormat=json&exportLayout=levels&exportAreas=water&union=false&selected=62422,62504,1739381,1739376,1739380,62405,1739377,62685,1431517,1739379,62607,62467,62366"
